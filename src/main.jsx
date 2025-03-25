82b47ba6d938ad850595b92af332430b32d567bd
imnxtamess <davide.morisco.work@gmail.com>
2025-03-25 16:47:08 +0100

    Fix import path for Main component to match casing

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Main from "./components/main";
+import Main from "./components/Main";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
